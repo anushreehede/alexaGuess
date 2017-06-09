e49e3d2692ae1998d89f6d371581d63f116e8cca
Anushree Hede <shuttler22@gmail.com>
2017-06-09 15:05:38 +0530

    First Alexa Skill with persistence - DynamoDB

---
+++ README.md
@@ -0,0 +1,8 @@
+# High-Low Guessing Game with Alexa SDK
+
+1. Clone this repository
+2. `cd` into this repository, i.e. `cd AlexaGuess`
+3. Run `npm install`
+4. Run `npm install alexa-sdk`
+5. Zip  `/src` and upload it to AWS Lambda
+6. Use files in `/speechAssets` for intents and utterances on the Amazon developer console
---
+++ src/index.js
@@ -0,0 +1,208 @@
+'use strict';
+// Importing Alexa SDK
+var Alexa = require('alexa-sdk')
+
+// Exporting the handler
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context, callback);
+    alexa.appId = appId; // What's this? 
+    alexa.dynamoDBTableName = 'highLowGuessUsers'; // Links session attributes to DB for persistence
+    alexa.registerHandlers(newSessionHandlers, guessModeHandlers, startGameHandlers); // register multiple handlers using this same method with ,
+    alexa.execute();
+};
+
+// State definitions 
+var states = {
+    GUESSMODE: '_GUESSMODE', // User is trying to guess the number.
+    STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
+};
+
+// Not bound to a state
+var newSessionHandlers = {
+
+     // This will short-cut any incoming intent or launch requests and route them to this handler.
+    'NewSession': function() {
+        if(Object.keys(this.attributes).length === 0) { // Check if it's the first time the skill has been invoked
+            this.attributes['endedSessionCount'] = 0;
+            this.attributes['gamesPlayed'] = 0;
+        }
+        this.handler.state = states.STARTMODE;
+        this.emit(':ask', 'Welcome to High Low guessing game. You have played '
+            + this.attributes['gamesPlayed'].toString() + ' times. Would you like to play?',
+            'Say yes to start the game or no to quit.');
+    }
+};
+
+// Handler associated with a state 
+var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
+
+    'NewSession': function () {
+        this.handler.state = '';
+        this.emitWithState('NewSession'); // Equivalent to the Start Mode NewSession handler
+    },
+
+    'NumberGuessIntent': function() {
+        var guessNum = parseInt(this.event.request.intent.slots.number.value);
+        var targetNum = this.attributes['guessNumber'];
+
+        console.log('user guessed: ' + guessNum);
+
+        if(guessNum > targetNum){
+            this.emit('TooHigh', guessNum);
+        } else if( guessNum < targetNum){
+            this.emit('TooLow', guessNum);
+        } else if (guessNum === targetNum){
+            // With a callback, use the arrow function to preserve the correct 'this' context
+            this.emit('JustRight', () => {
+                this.emit(':ask', guessNum.toString() + 'is correct! Would you like to play a new game?',
+                'Say yes to start a new game, or no to end the game.');
+            });
+        } else {
+            this.emit('NotANum');
+        }
+    },
+
+    'AMAZON.HelpIntent': function() {
+        this.emit(':ask', 'I am thinking of a number between zero and one hundred, try to guess and I will tell you' +
+            ' if it is higher or lower.', 'Try saying a number.');
+    },
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        this.emit(':saveState', true); // Be sure to call :saveState to persist your session attributes in DynamoDB
+    },
+
+    'Unhandled': function() {
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+    }
+
+});
+
+// Handler associated with a state 
+var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
+
+    'NewSession': function () {
+        this.emit('NewSession'); // Uses the handler in newSessionHandlers
+    },
+
+    'AMAZON.HelpIntent': function() {
+        var message = 'I will think of a number between zero and one hundred, try to guess and I will tell you if it' +
+            ' is higher or lower. Do you want to start the game?';
+        this.emit(':ask', message, message);
+    },
+
+    'AMAZON.YesIntent': function() {
+        this.attributes['guessNumber'] = Math.floor(Math.random() * 100);
+        this.handler.state = states.GUESSMODE;
+        this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
+    },
+
+    'AMAZON.NoIntent': function() {
+        this.emit(':tell', 'Ok, see you next time!');
+    },
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        this.emit(':saveState', true);
+    },
+
+    'Unhandled': function() {
+        var message = 'Say yes to continue, or no to end the game.';
+        this.emit(':ask', message, message);
+    }
+});
+
+// Not bound to a state 
+var guessAttemptHandlers = {
+    'TooHigh': function(val) {
+        this.emit(':ask', val.toString() + ' is too high.', 'Try saying a smaller number.');
+    },
+    'TooLow': function(val) {
+        this.emit(':ask', val.toString() + ' is too low.', 'Try saying a larger number.');
+    },
+    'JustRight': function(callback) {
+        this.handler.state = states.STARTMODE;
+        this.attributes['gamesPlayed']++;
+        callback();
+    },
+    'NotANum': function() {
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+    }
+};
+
+/*
+to access session attributes via DB
+
+this.attributes['yourAttribute'] = 'value';
+
+You can create the table manually beforehand or simply give your Lambda function DynamoDB create table permissions and it will happen automatically.
+*/
+// generic handlers defnition 
+/*
+var handlers = {
+    // Register the handler functions for all intents
+    "HelloWorldIntent": function(){
+        this.emit(':tell', 'Hello, World!');
+    },
+    "LaunchRequest": function(){
+        this.emit('HelloWorldIntent'); // handlers can call each other
+    }
+};
+*/
+// Common skill responses and options!
+/*
+var speechOutput = 'Hello world!';
+var repromptSpeech = 'Hello again!';
+
+this.emit(':tell', speechOutput);
+
+this.emit(':ask', speechOutput, repromptSpeech);
+
+var cardTitle = 'Hello World Card';
+var cardContent = 'This text will be displayed in the companion app card.';
+
+var imageObj = {
+    smallImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
+    largeImageUrl: 'https://imgs.xkcd.com/comics/standards.png'
+};
+
+var permissionArray = ['read::alexa:device:all:address'];
+
+var updatedIntent = this.event.request.intent;
+
+var slotToElicit = "Slot to elicit";
+
+var slotToConfirm = "Slot to confirm";
+
+this.emit(':askWithCard', speechOutput, repromptSpeech, cardTitle, cardContent, imageObj);
+
+this.emit(':tellWithCard', speechOutput, cardTitle, cardContent, imageObj);
+
+this.emit(':tellWithLinkAccountCard', speechOutput);
+
+this.emit(':askWithLinkAccountCard', speechOutput);
+
+this.emit(':tellWithPermissionCard', speechOutput, permissionArray);
+
+this.emit(':delegate', updatedIntent);
+
+this.emit(':elicitSlot', slotToElicit, speechOutput, repromptSpeech, updatedIntent);
+
+this.emit(':elicitSlotWithCard', slotToElicit, speechOutput, repromptSpeech, cardTitle, cardContent, updatedIntent, imageObj);
+
+this.emit(':confirmSlot', slotToConfirm, speechOutput, repromptSpeech, updatedIntent);
+
+this.emit(':confirmSlotWithCard', slotToConfirm, speechOutput, repromptSpeech, cardTitle, cardContent, updatedIntent, imageObj);
+
+this.emit(':confirmIntent', speechOutput, repromptSpeech, updatedIntent);
+
+this.emit(':confirmIntentWithCard', speechOutput, repromptSpeech, cardTitle, cardContent, updatedIntent, imageObj);
+
+this.emit(':responseReady'); // Called after the response is built but before it is returned to the Alexa service. Calls :saveState. Can be overridden.
+
+this.emit(':saveState', false); // Handles saving the contents of this.attributes and the current handler state to DynamoDB and then sends the previously built response to the Alexa service. Override if you wish to use a different persistence provider. The second attribute is optional and can be set to 'true' to force saving.
+
+this.emit(':saveStateError'); // Called if there is an error while saving state. Override to handle any errors yourself.
+*/
